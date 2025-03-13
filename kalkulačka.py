a7a3b2b71df093fdc4a4fad61bd8ea5a47513dc5
Dominik <mikuleckydominik@gmail.com>
2025-03-13 19:09:39 +0100

    vložení entry tlačítka

--- kalkulačka.py
+++ kalkulačka.py
@@ -2,10 +2,14 @@ from tkinter import *
 okno = Tk()
 
 okno.title("Kalkulačka")
-okno.geometry("400x400+500+300")
+okno.iconbitmap("ikonka.ico")
+okno.geometry("500x500+500+300")
 okno.resizable(False,False)
+okno.config(bg = "#4C5958")
+main_font = ("Helvetice",20)
 
 
-
+davat = Entry(okno, width=30, borderwidth= 5, relief="sunken",font = main_font, justify="right", background="#10403B", fg = "white" )
+davat.grid(column=0, row=0, padx=10, pady=10, ipadx=10, ipady=10)
 
 okno.mainloop()
